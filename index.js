0faa0281fc4b12424caadc6aeb988efd39088aa5
Leon Gaban <leon.gaban@yum.com>
2018-01-22 11:24:47 -0600

    setup basic server

--- index.js
+++ index.js
@@ -6,3 +6,7 @@ const server = http.createServer((req, res) => {
   res.setHeader('Content-Type', 'text/plain');
   res.end('Hello World!\n');
 });
+
+server.listen(port, hostname, () => {
+  console.log(`Server is running at http://${hostname}:${port}`);
+});
